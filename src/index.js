770ce372be366470cb13fa0da6417d2e965db2ae
deniseprosser <denise.prosser23@gmail.com>
2024-05-20 14:44:54 +0100

    Added basic form handling in and added typewriter in JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe-text", {
+    strings: "Generating recipe...",
+    autoStart: true,
+    cursor: null,
+    delay: 50,
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
