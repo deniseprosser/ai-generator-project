29fe8d801a224c593d148475fa1c927de7a577ec
deniseprosser <denise.prosser23@gmail.com>
2024-11-19 13:46:42 +0000

    Fixed bug in AI response

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateRecipe(event) {
   let apiKey = "aab3d3obc0205ebt561454f09dff85ec";
   let prompt = `user instructions: create a recipe using the ingredients from ${promptInput.value}`;
   let context =
-    "you are an expert chef who can create simple and delicious recipes from any cuisine. Your mission is to create a simple recipe using the ingredients listed in the user instructions. Make sure to follow the user instructions. The recipe must be provided in HTML format. Example: <p>This is a recipe</p>";
+    "you are an expert chef who can create simple and delicious recipes from any cuisine. Your mission is to create a simple recipe using the ingredients listed in the user instructions. Make sure to follow the user instructions. Present the recipe in basic HTML format. Do not include '```html` at the start of the answer";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe-text");
